4e24abc37d263f8cca965ef8820757dc1d741ff8
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-10 17:08:55 +0100

    added items in domain, customer as a template

---
+++ src/main/java/com/qa/ims/persistence/domain/Items.java
@@ -0,0 +1,82 @@
+package com.qa.ims.persistence.domain;
+
+public class Items {
+	private Long id;
+	private String itemName;
+	private float price;
+
+	public Items(String itemName, float price) {
+		this.setItemName(itemName);
+		this.setPrice(price);
+	}
+
+	public Items(Long id, String itemName, float price) {
+		this.setId(id);
+		this.setItemName(itemName);
+		this.setPrice(price);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " item name:" + itemName + " price:" + price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + Float.floatToIntBits(price);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Items other = (Items) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemName == null) {
+			if (other.itemName != null)
+				return false;
+		} else if (!itemName.equals(other.itemName))
+			return false;
+		if (Float.floatToIntBits(price) != Float.floatToIntBits(other.price))
+			return false;
+		return true;
+	}
+
+}
