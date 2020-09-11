56f779b249c89d53c2f349335ff16271b16bb29d
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-11 15:03:19 +0100

    created Order class using template

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,122 @@
+package com.qa.ims.persistence.domain;
+
+public class Order {
+	
+	private Long id;
+	private Long customerId;
+	private Long itemId;
+	private Integer quantity;
+	
+	
+	public Order(Long customerId, Long itemId, Integer quantity) {
+		super();
+		this.customerId = customerId;
+		this.itemId = itemId;
+		this.quantity = quantity;
+	}
+
+
+	public Order(Long id, Long customerId, Long itemId, Integer quantity) {
+		super();
+		this.id = id;
+		this.customerId = customerId;
+		this.itemId = itemId;
+		this.quantity = quantity;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+
+	public Long getItemId() {
+		return itemId;
+	}
+
+
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", customerId=" + customerId + ", itemId=" + itemId + ", quantity=" + quantity + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customerId == null) ? 0 : customerId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (customerId == null) {
+			if (other.customerId != null)
+				return false;
+		} else if (!customerId.equals(other.customerId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+
+}
