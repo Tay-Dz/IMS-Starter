75a02dd7eff2869585144d2e697b58d22eb654e8
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-11 14:43:19 +0100

    fixed sql data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,5 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('steve', 'jones');
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
-INSERT INTO `ims`.`items` (`first_name`, `surname`) VALUES ('book', 6.99);
-INSERT INTO `ims`.`items` (`first_name`, `surname`) VALUES ('PC', 549.99);
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('book', 6.99);
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('PC', 549.99);
