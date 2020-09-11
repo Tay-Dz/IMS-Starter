930ab23a6f8658c089f991f3ba2386d37f4b79aa
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-11 13:13:09 +0100

    created ItemsController using customersController as a template

---
+++ src/main/java/com/qa/ims/controller/ItemsController.java
@@ -0,0 +1,82 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemsDAO;
+import com.qa.ims.persistence.domain.Items;
+import com.qa.ims.utils.Utils;
+
+public class ItemsController implements CrudController<Items> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemsDAO itemDAO;
+	private Utils utils;
+
+	public ItemsController(ItemsDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all customers to the logger
+	 */
+	@Override
+	public List<Items> readAll() {
+		List<Items> items = itemDAO.readAll();
+		for (Items item : items) {
+			LOGGER.info(item.toString());
+		}
+		return items;
+	}
+
+	/**
+	 * Creates a customer by taking in user input
+	 */
+	@Override
+	public Items create() {
+		LOGGER.info("Please enter a product name");
+		String itemName = utils.getString();
+		LOGGER.info("Please enter a price");
+		double price = utils.getDouble();
+		Items item = itemDAO.create(new Items(itemName, price));
+		LOGGER.info("Item created");
+		return item;
+	}
+
+	/**
+	 * Updates an existing customer by taking in user input
+	 */
+	@Override
+	public Items update() {
+		LOGGER.info("Please enter the id of the item you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter the item name");
+		String itemName = utils.getString();
+		LOGGER.info("Please enter the item price");
+		Double price = utils.getDouble();
+		Items item = itemDAO.update(new Items(id, itemName, price));
+		LOGGER.info("Item Updated");
+		return item;
+	}
+
+	/**
+	 * Deletes an existing customer by the id of the customer
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the item you would like to delete");
+		Long id = utils.getLong();
+		return itemDAO.delete(id);
+	}
+
+}
+
+
+
