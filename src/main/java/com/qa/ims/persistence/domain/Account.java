8b0a7ce5dde664c888044b8c3f02e2048f16c147
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-15 15:35:39 +0100

    added id to Account class

--- src/main/java/com/qa/ims/persistence/domain/Account.java
+++ src/main/java/com/qa/ims/persistence/domain/Account.java
@@ -2,15 +2,28 @@ package com.qa.ims.persistence.domain;
 
 public class Account {
 
+	private Long id;
 	private String userName;
 	private String password;
 	private Boolean isAdmin;
 
-	public Account(String userName, String password, Boolean isAdmin) {
+	public Account(Long id,String userName, String password, Boolean isAdmin) {
+		this.setId(id);
 		this.setUserName(userName);
 		this.setPassword(password);
 		this.setIsAdmin(isAdmin);
 	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getUserName() {
 		return userName;
@@ -36,20 +49,24 @@ public class Account {
 		this.isAdmin = isAdmin;
 	}
 
+
 	@Override
 	public String toString() {
-		return "Account [userName=" + userName + ", password=" + password + ", isAdmin=" + isAdmin + "]";
+		return "Account [id=" + id + ", userName=" + userName + ", password=" + password + ", isAdmin=" + isAdmin + "]";
 	}
+
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((isAdmin == null) ? 0 : isAdmin.hashCode());
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -60,6 +77,11 @@ public class Account {
 		if (getClass() != obj.getClass())
 			return false;
 		Account other = (Account) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
 		if (isAdmin == null) {
 			if (other.isAdmin != null)
 				return false;
@@ -77,5 +99,7 @@ public class Account {
 			return false;
 		return true;
 	}
+
+	
 
 }
