e604e1a6e6468aa0bf9278766f34acaf3d1a52ce
Tay-Dzonu-QA <tdzonu@qa.com>
2020-09-16 11:14:05 +0100

    updated test sql info

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,9 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('book', 6.99);
+
+INSERT INTO `ims`.`order_customer` (`customer_id`) VALUES (1);
+
+INSERT INTO `ims`.`order_products` (`id`,`item_id`,`quantity`) VALUES (1,1,2);
+
+INSERT INTO `ims`.`accounts` (`user_name`, `password`,`is_admin`) VALUES ('root', 'root',true);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,7 +1,41 @@
 DROP TABLE IF EXISTS `customers`;
-CREATE TABLE IF NOT EXISTS `customers` (
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) NULL DEFAULT NULL,
     `surname` VARCHAR(40) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `items`;
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) NULL DEFAULT NULL,
+    `price` DOUBLE(5,2) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `order_customer`;
+CREATE TABLE IF NOT EXISTS `ims`.`order_customer` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`customer_id` INT(11) NOT NULL,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`)
+);
+
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE IF NOT EXISTS `ims`.`order_products` (
+	`id` INT(11) NOT NULL,
+	`item_id` INT(11) NOT NULL,
+	`quantity` INT(11) NOT NULL,
+	FOREIGN KEY (`id`) REFERENCES `order_customer`(`id`),
+	FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
+);
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE IF NOT EXISTS `ims`.`accounts` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `user_name` VARCHAR(40) NULL DEFAULT NULL,
+    `password` VARCHAR(40) NULL DEFAULT NULL,
+    `is_admin` BOOLEAN NULL DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
